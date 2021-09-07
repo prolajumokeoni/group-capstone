7192ab44a74ad77e8536f810094bfa0dfb706e1c
Daniel M. Matongo <m_matongo@icloud.com>
2021-09-07 14:59:15 +0200

    update changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './styles.css';
 
-import { displayMovies } from './movies';  
+import displayMovies from './movies';
 
 document.addEventListener('DOMContentLoaded', () => { displayMovies(); });
