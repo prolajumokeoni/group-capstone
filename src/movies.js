2fa1fcac3cbd91a286999eaf5f2c68cd9b3f77d8
Daniel M. Matongo <m_matongo@icloud.com>
2021-09-07 14:59:07 +0200

    update changes

--- src/movies.js
+++ src/movies.js
@@ -1,23 +1,23 @@
-import { pullMovies } from "./api";
+import { pullMovies } from './api';
 
 const getCount = () => {
-    const movieCount = document.getElementById('movies').children.length;
-    return movieCount;
+  const movieCount = document.getElementById('movies').children.length;
+  return movieCount;
 };
 
 const updateTitles = (count) => {
-    const movieTitle = document.getElementById('movie-count');
-    movieTitle.innerText = `Movies (${count})`;
+  const movieTitle = document.getElementById('movie-count');
+  movieTitle.innerText = `Movies (${count})`;
 };
 
 const displayMovies = async () => {
-    const movies = await pullMovies();
-    const moviesList = document.getElementById('movies');
+  const movies = await pullMovies();
+  const moviesList = document.getElementById('movies');
 
-    moviesList.innerHTML = "";
-    for (let i = 0; i < movies.length; i += 1) {
-        const image = movies[i].show.image?.medium ?? 'https://i.ibb.co/nPzyFm6/placeholder.png';
-        moviesList.insertAdjacentHTML('beforeend', ` 
+  moviesList.innerHTML = '';
+  for (let i = 0; i < movies.length; i += 1) {
+    const image = movies[i].show.image?.medium ?? 'https://i.ibb.co/nPzyFm6/placeholder.png';
+    moviesList.insertAdjacentHTML('beforeend', ` 
             <div class="movie-container">
                 <div class="movie-content">
                 <img src="${image}" />
@@ -26,10 +26,10 @@ const displayMovies = async () => {
                     <h2>${movies[i].show.name}</h2>
                 </div>
         </div>`);
-    }
+  }
 
-    const movieCount = getCount();
-    updateTitles(movieCount);
-}
+  const movieCount = getCount();
+  updateTitles(movieCount);
+};
 
-export { displayMovies };
+export default displayMovies;
