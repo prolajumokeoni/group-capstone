c4118d26ac768a938ca4a3443e6366b37e154e56
Daniel M. Matongo <m_matongo@icloud.com>
2021-09-09 10:17:12 +0200

    update like count logic

--- src/movies.js
+++ src/movies.js
@@ -37,7 +37,7 @@ const displayMovies = async () => {
                 </div>
                 <button data-id="${movies[i].show.id}" class="material-icons-outlined">mode_comment</button>
                 <span like-id="${movies[i].show.id}" class="material-icons-outlined">thumb_up</span>
-        </div>`);
+            </div>`);
     const button = document.querySelectorAll(`[data-id="${movies[i].show.id}"]`)[0];
     button.addEventListener('click', (e) => {
       const movieId = e.target.getAttribute('data-id');
@@ -45,20 +45,19 @@ const displayMovies = async () => {
     });
     const likeButton = document.querySelectorAll(`[like-id="${movies[i].show.id}"]`)[0];
     likeButton.addEventListener('click', async (e) => {
-      const movieId = e.target.parentElement.parentElement.getAttribute('like-id');
+      const movieId = e.target.getAttribute('like-id');
       const status = await addLike(Number(movieId));
       const newLikes = await pullLikes(movieId);
-      const likesObject = newLikes.filter((like) => like.item_id === movies[i].show.id);
-      const likeCount = `${likesObject[0].likes} likes`;
-      if (status === 201) {
-        const likeDisplay = likeButton.previousElementSibling.previousElementSibling.children[1];
-        likeDisplay.innerText = likeCount;
-      }
+      const newLikeObject = newLikes.filter((like) => like.item_id === Number(movieId));
+      const newNumLikes = newLikeObject[0].likes;
+      const newLike = document.querySelectorAll(`[like-id="${movieId}"]`)[0];
+      status.status === '201' ? newLike.innerText = `${newNumLikes} likes` : newLike.innerText = `${newNumLikes} likes`;
+      window.location.reload();
     });
+  };
 
-    const movieCount = getCount();
-    updateTitles(movieCount);
-  }
+  const movieCount = getCount();
+  updateTitles(movieCount);
 };
 
 export default displayMovies;
